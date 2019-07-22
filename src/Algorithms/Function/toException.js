c095359331dc355a29843e31a2c1d5c44bf93ee1
ace411 <lochbm@live.com>
2019-07-22 23:05:01 +0300

    Added exception.message check

--- src/Algorithms/Function/toException.js
+++ src/Algorithms/Function/toException.js
@@ -1,9 +1,13 @@
+const isUndefined = require('./isUndefined')
+
 const toException = (func) => {
     return (...args) => {
         try {
             return func(...args)
         } catch (exception) {
-            return exception
+            return isUndefined(exception.message) 
+                ? exception 
+                : exception.message
         }
     }
 }
