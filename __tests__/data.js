e783e8346a2239a0118c6933f5ef5cff372d9b7a
ace411 <lochbm@live.com>
2019-07-23 19:57:58 +0300

    Added test data

--- __tests__/data.js
+++ __tests__/data.js
@@ -7,6 +7,22 @@ const argObj = {
     func: (x) => x / 2
 }
 
+const strArr = ['foo', 'bar']
+
+const strObj = { a: 'foo', b: 'bar' }
+
+const numArr = [1, 2, 3, 4]
+
+const numObj = { a: 12, b: 13, c: 99 }
+
 const factorial = (x) => x < 2 ? 1 : x * factorial(x - 1)
 
-module.exports = { argObj, quadEq, factorial }
+module.exports = { 
+    argObj, 
+    quadEq, 
+    factorial, 
+    strArr, 
+    strObj, 
+    numArr, 
+    numObj 
+}
