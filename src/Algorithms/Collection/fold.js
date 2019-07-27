ca1aa21086bbbbfeb6404c72f1f7ef4317958451
ace411 <lochbm@live.com>
2019-07-28 00:42:24 +0300

    Modified fold function

--- src/Algorithms/Collection/fold.js
+++ src/Algorithms/Collection/fold.js
@@ -1,10 +1,9 @@
 const fold = (func, list, acc) => {
-    let surface = Array.isArray(list) 
-        ? list 
-        : Object.keys(list)
+    let isArr = Array.isArray(list)
+    let surface = isArr ? list : Object.keys(list)
 
     for (let x = 0; x < surface.length; x++) {
-        acc = Array.isArray(list)
+        acc = isArr
             ? func(acc, surface[x])
             : func(acc, list[surface[x]])
     }
