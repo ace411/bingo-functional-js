4832ed312bd615ed75e6bbee64f702ba5815815c
ace411 <lochbm@live.com>
2019-07-29 20:58:42 +0300

    Modified function body

--- src/Algorithms/Collection/indexOf.js
+++ src/Algorithms/Collection/indexOf.js
@@ -1,15 +1,19 @@
 const head = require('./head')
+const { isNumeric } = require('../Function')
 
 const indexOf = (haystack, needle) => {
-    let isArray = Array.isArray(haystack)
-    let search = isArray ? haystack : Object.keys(haystack)
     let index = []
 
-    for (let acc = 0; acc < search.length; acc++) {
-        if (isArray) {
-            needle === haystack[acc] && index.push(acc)
+    for (let property in haystack) {
+        let val = haystack[property]
+
+        if (val === needle) {
+            index.push(
+                isNumeric(property)
+                    ? Number.parseInt(property)
+                    : property
+            )
         }
-        needle === haystack[search[acc]] && index.push(search[acc])
     }
 
     return head(index)
