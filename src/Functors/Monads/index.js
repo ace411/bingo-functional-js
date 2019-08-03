b3010edec6701c05bd61acf18c8d07fccd0135b6
ace411 <lochbm@live.com>
2019-08-03 05:59:11 +0300

    Created Writer monad

--- src/Functors/Monads/index.js
+++ src/Functors/Monads/index.js
@@ -2,5 +2,12 @@ const Monadic = require('./monad')
 const IO = require('./IO')
 const Reader = require('./Reader')
 const State = require('./State')
+const Writer = require('./Writer')
 
-module.exports = { Monadic, ...IO, ...Reader, ...State }
+module.exports = {
+    Monadic,
+    ...IO,
+    ...Reader,
+    ...State,
+    ...Writer
+}
