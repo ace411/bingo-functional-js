883bc1a28237280c00862c890b5746cd34ac5b4a
ace411 <lochbm@live.com>
2019-08-22 22:14:42 +0300

    Modified function

--- src/Algorithms/Collection/keyExists.js
+++ src/Algorithms/Collection/keyExists.js
@@ -1,26 +1,18 @@
 const head = require('./head')
-const sizeOf = require('./sizeOf')
-const { isUndefined, compose } = require('../Function')
+const { isUndefined } = require('../Function')
 
 const keyExists = (list, key) => {
     let ret = []
-    let check = compose(head, res => isUndefined(res) ? false : res)
-
-    if (Array.isArray(list)) {
-        for (let acc = 0; acc < sizeOf(list); acc++) {
-            if (key === acc) {
-                ret.push(true)
-            }
-        }
-    }
 
     for (let property in list) {
-        if (key === property) {
+        if (key.toString() === property) {
             ret.push(true)
         }
     }
 
-    return check(ret)
+    let res = head(ret)
+
+    return !isUndefined(res) ? res : false
 }
 
 module.exports = keyExists
