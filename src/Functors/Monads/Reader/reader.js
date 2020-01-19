0723861705e4c564724e1d83bba8600f8bad2bd5
ace411 <lochbm@live.com>
2020-01-19 05:04:38 +0300

    Added Reader monad function doc blocks

--- src/Functors/Monads/Reader/reader.js
+++ src/Functors/Monads/Reader/reader.js
@@ -1,22 +1,78 @@
 const Monadic = require('../monad')
 const { isFunction, constantFunction } = require('../../../Algorithms/Function')
 
+/**
+ * Reader monad
+ * @constructor
+ * @param {function} operation
+ */
 function Reader(operation) {
   Monadic.call(this, isFunction(operation) ? operation : constantFunction(operation))
+
+  /**
+   * ap function
+   * @param {Reader} reader
+   * @returns {Reader}
+   * @example
+   *
+   * Reader.of((fst) => (last) => Reader.of(`Hello, ${first} ${last}`))
+   *  .ap(Reader.of('World'))
+   * // => [Reader]
+   */
   this.ap = function (reader) {
-    return this.bind((func) => reader.map(func))
+    return this.bind((modify) => reader.map(modify))
   }
-  this.bind = function (func) {
-    return new Reader((env) => func(this.run(env)).run(env))
+
+  /**
+   * bind function
+   * @param {function} computation
+   * @returns {Reader}
+   * @example
+   *
+   * Reader.of((name) => `Hello ${name}`)
+   *  .bind((greet) => Reader.of((name) => `${greet} ${name === 'world' && 'hey'}`))
+   * // => [Reader]
+   */
+  this.bind = function (computation) {
+    return new Reader((env) => computation(this.run(env)).run(env))
   }
-  this.map = function (func) {
-    return this.bind((env) => new Reader(func(env)))
+
+  /**
+   * map function
+   * @param {function} computation
+   * @returns {Reader}
+   * @example
+   *
+   * Reader.of(identity).map(x => Reader.of(x ** 2))
+   * // => [Reader]
+   */
+  this.map = function (computation) {
+    return this.bind((env) => new Reader(computation(env)))
   }
+
+  /**
+   * run function
+   * @param {*} env
+   * @returns {Reader}
+   * @example
+   *
+   * Reader.of(identity).run(2)
+   * // => 2
+   */
   this.run = function (env) {
     return this.val(env)
   }
 }
 
+/**
+ * of function
+ * @param {function} operation
+ * @returns {Reader}
+ * @example
+ *
+ * Reader.of(identity)
+ * // => [Reader]
+ */
 Reader.of = function (operation) {
   return new Reader(operation)
 }
