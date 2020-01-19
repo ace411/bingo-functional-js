b8c11083f2ba7a018113e6fb6b9a04eb23223c08
ace411 <lochbm@live.com>
2020-01-19 03:20:10 +0300

    Added doc blocks

--- src/Functors/Monads/IO/functions.js
+++ src/Functors/Monads/IO/functions.js
@@ -1,6 +1,18 @@
 const { IO: _IO } = require('./io')
 const { toException, compose, isFunction } = require('../../../Algorithms/Function')
 
+/**
+ * IO function
+ * initialize a value of type IO
+ *
+ * IO :: (a -> b) -> IO ()
+ * @param {function} operation
+ * @returns {IO}
+ * @example
+ *
+ * IO(() => fs.readFileSync('data.txt', { encoding: 'utf-8' }))
+ * // => [IO]
+ */
 const IO = (operation) => _IO.of(operation)
 
 function _IOException(message) {
@@ -8,10 +20,32 @@ function _IOException(message) {
   this.name = 'IOException'
 }
 
+/**
+ * IOException function
+ * throws an IO exception
+ * IOException :: String -> IO ()
+ * @param {string} msg
+ * @returns {IO}
+ * @example
+ *
+ * IOException('fail')
+ * // => [IO]
+ */
 const IOException = (msg) => IO(() => () => {
   throw new _IOException(msg)
 })
 
+/**
+ * catchIO function
+ * catches an IO exception
+ * catchIO :: IO a -> (IOException -> IO a) -> IO a
+ * @param {object} io
+ * @returns {IO}
+ * @example
+ *
+ * catchIO(IOException('fail'))
+ * // => [IO]
+ */
 const catchIO = (io) => io.bind((operation) => {
   const ret = compose(toException, IO)
 
--- src/Functors/Monads/IO/io.js
+++ src/Functors/Monads/IO/io.js
@@ -1,29 +1,95 @@
 const Monadic = require('../monad')
 const { isFunction, constantFunction } = require('../../../Algorithms/Function')
 
+/**
+ * IO monad
+ * @constructor
+ * @param {function} unsafe
+ */
 function IO(unsafe) {
   Monadic.call(this, isFunction(unsafe) ? unsafe : constantFunction(unsafe))
+
+  /**
+   * ap function
+   * @param {IO} io
+   * @returns {IO}
+   * @example
+   *
+   * IO.of(() => (str) => str.toUpperCase()).ap(IO.of(() => 'foo'))
+   * // => [IO]
+   */
   this.ap = function (io) {
     const ret = this.exec()
     return io.map(ret)
   }
+
+  /**
+   * bind function
+   * @param {function} operation
+   * @returns {IO}
+   * @example
+   *
+   * IO.of(() => fs.readFileSync('data.txt', { encoding: 'utf-8' }))
+   *  .bind((str) => IO.of(str.toLowerCase()))
+   * // => [IO]
+   */
   this.bind = function (operation) {
     const ret = this.exec()
     return operation(ret)
   }
+
+  /**
+   * map function
+   * @param {function} operation
+   * @returns {IO}
+   * @example
+   *
+   * IO.of(() => fs.readFileSync('data.txt', { encoding: 'utf-8' }))
+   *  .map(slugify)
+   * // => [IO]
+   */
   this.map = function (operation) {
     const ret = this.exec()
     return new IO(operation(ret))
   }
-  this.flatMap = function (func) {
+
+  /**
+   * flatMap function
+   * @param {function} operation
+   * @returns {*}
+   * @example
+   *
+   * IO.of(() => fs.readFileSync('data.txt', { encoding: 'utf-8' }))
+   *  .map(slugify)
+   * // => 'file-contents'
+   */
+  this.flatMap = function (operation) {
     const ret = this.exec()
-    return func(ret)
+    return operation(ret)
   }
+
+  /**
+   * exec function
+   * @returns {*}
+   * @example
+   *
+   * IO.of(() => fs.readFileSync('data.txt', { encoding: 'utf-8' })).exec()
+   * // => 'file contents'
+   */
   this.exec = function () {
     return (this.val)()
   }
 }
 
+/**
+ * of function
+ * @param {function} operation
+ * @returns {IO}
+ * @example
+ *
+ * IO.of(() => fs.readFileSync('data.txt', { encoding: 'utf-8' }))
+ * // => [IO]
+ */
 IO.of = function (operation) {
   return new IO(operation)
 }
